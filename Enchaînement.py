1ac8abd91928e842c1c7035eb37e5dbcd08babb0
HugolDev <79276855+HugolDev@users.noreply.github.com>
2021-11-21 00:14:02 +0100

    Add files via upload

---
+++ Enchaînement.py
@@ -0,0 +1,14 @@
+# On crée la variable birthday puis on affecte.
+birthday_string ="I am"
+
+# On crée une variable age et on donne une valeur
+age = 23
+
+# On crée la variable birthday puis on affecte.
+birthday_string_2 = "years old!"
+
+# Concaténer un entier avec des chaînes de caractères est possible si nous transformons l'entier en chaîne de caractères en premier lieu.
+full_birthday_string = birthday_string + str(age) + birthday_string_2
+
+# Puis on affiche la variable
+print(full_birthday_string)
